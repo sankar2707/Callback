5eb7a632e89f0ddc6007812eddd3d1c601fa6d73
sankar2707 <sankar.r2707@gmail.com>
2024-05-08 17:40:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const countdown=document.querySelector(".countdown") 
+
+let time=10;                                       
+
+countdown.innerHTML=time--;                         
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+setTimeout(()=>{
+countdown.innerHTML=time--;
+countdown.innerHTML="Happy Independence Day🎉"
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000);
+},1000)
